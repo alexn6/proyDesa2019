206aa8a87a65e38eb755a3f57aa9d7a3039be2d7
alex <alex6tc90@gmail.com>
2019-10-22 00:11:28 -0300

    Incorporado nuevo proyecto firebase con datos de la app de produccion.

--- src/Controller/NotificacionController.php
+++ src/Controller/NotificacionController.php
@@ -61,8 +61,8 @@ class NotificacionController extends AbstractFOSRestController
     {
         $URL  = "https://fcm.googleapis.com/fcm/send";  //API URL of FCM
     
-        $API_ACCESS_KEY_SERVER = 'AAAA9P-hCPk:APA91bHBrbHBDxwmtD6RdwK-MvyxV17WUmqsyHV5VTOgTL5hfTbUiiBV2x88Ywqj0Y51di-VaDB7CS2ihHns1Hjj5l-DAcrDseJaWommLVR47y2vMO9Iw4nRYgKTtc77J0ej8nF2QYjr'; // YOUR_FIREBASE_API_KEY
-        //$TOKEN_DEVICE = 'fOYrDHi4IKQ:APA91bEb_W4aV9AsWQO5clEOJhjhLW362IMyvYAZtOXR4JU--zuwNTfAwrf9Pb3Zg_TWbUek7ctn2bulMOYjL_1c2B4T6f2Rk9fy2qM-x09kx9iR7KsbnVV_Teea-lMwtOZwERAbvloD';
+        //$API_ACCESS_KEY_SERVER = 'AAAA9P-hCPk:APA91bHBrbHBDxwmtD6RdwK-MvyxV17WUmqsyHV5VTOgTL5hfTbUiiBV2x88Ywqj0Y51di-VaDB7CS2ihHns1Hjj5l-DAcrDseJaWommLVR47y2vMO9Iw4nRYgKTtc77J0ej8nF2QYjr'; // YOUR_FIREBASE_API_KEY
+        $API_ACCESS_KEY_SERVER = 'AAAAIv70EkE:APA91bGetKCqMPfkVuRLn7nT9qqMgUdzc9mN5lB-ny9_XX1gQjdITjfcFE2NxPC_3I3c43XzwcVb8Y6RvT5I55hhScGpT8zDfkWKdFcXdlTCDhTHJo42ahHF-PI4bhrOcfxUqGrRz-lT';
         $TOKEN_DEVICE = $tokenDev;
 
         $notif = (object) null;
