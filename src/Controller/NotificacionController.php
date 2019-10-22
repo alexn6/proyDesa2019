0b34e0b69c60334eec7b9f520a95aa0d741acb5f
alex <alex6tc90@gmail.com>
2019-10-22 07:35:14 -0300

    Refactor de servicio de notificacion.

--- src/Controller/NotificacionController.php
+++ src/Controller/NotificacionController.php
@@ -8,7 +8,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
-//use App\Entity\UsuarioCompetencia;
+use App\Utils\NotificationService;
 
  /**
  * Notificacion controller
@@ -34,7 +34,9 @@ class NotificacionController extends AbstractFOSRestController
             $bodyNotification = $requestBody->body;
             $tokenDevice = $requestBody->token;
 
-            $resultSend = $this->sendNotificationFCM($titleNotification, $bodyNotification, $tokenDevice);
+            $servNotification = new NotificationService();
+            $resultSend = $servNotification->sendSimpleNotificationFCM($titleNotification, $tokenDevice, $bodyNotification);
+
             $resultSend = json_decode($resultSend, true);
 
             $statusCode = Response::HTTP_OK;
@@ -54,54 +56,4 @@ class NotificacionController extends AbstractFOSRestController
         return $response;
     }
 
-
-    // ############################################################################
-    // ###################### FUNCIONES AUXILIARES ############################
-    private function sendNotificationFCM($titleNotif, $bodyNotif, $tokenDev)
-    {
-        $URL  = "https://fcm.googleapis.com/fcm/send";  //API URL of FCM
-    
-        //$API_ACCESS_KEY_SERVER = 'AAAA9P-hCPk:APA91bHBrbHBDxwmtD6RdwK-MvyxV17WUmqsyHV5VTOgTL5hfTbUiiBV2x88Ywqj0Y51di-VaDB7CS2ihHns1Hjj5l-DAcrDseJaWommLVR47y2vMO9Iw4nRYgKTtc77J0ej8nF2QYjr'; // YOUR_FIREBASE_API_KEY
-        $API_ACCESS_KEY_SERVER = 'AAAAIv70EkE:APA91bGetKCqMPfkVuRLn7nT9qqMgUdzc9mN5lB-ny9_XX1gQjdITjfcFE2NxPC_3I3c43XzwcVb8Y6RvT5I55hhScGpT8zDfkWKdFcXdlTCDhTHJo42ahHF-PI4bhrOcfxUqGrRz-lT';
-        $TOKEN_DEVICE = $tokenDev;
-
-        $notif = (object) null;
-        // $notif->title = "Portugal vs. Denmark";
-        // $notif->body = "El cuerpo de la notif";
-        $notif->title = $titleNotif;
-        $notif->body = $bodyNotif;
-
-        $msg = (object) null;
-        $msg->token = $TOKEN_DEVICE;
-        $msg->notification = $notif;
-
-        // dato a mandar
-        $response = (object) null;
-        $response->message = $msg;
-
-        $fields = array('to' => $TOKEN_DEVICE, 'data' => $response);
-
-        // print_r($response);
-        // echo(json_encode($response));
-
-        $headers = array('Authorization: key=' . $API_ACCESS_KEY_SERVER, 'Content-Type: application/json');
-
-        #Send Reponse To FireBase Server    
-        $ch = curl_init(); 
-        curl_setopt($ch,CURLOPT_URL, $URL);
-        curl_setopt($ch,CURLOPT_POST, true);
-        curl_setopt($ch,CURLOPT_HTTPHEADER, $headers);
-        curl_setopt($ch,CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch,CURLOPT_SSL_VERIFYPEER, false);
-        curl_setopt($ch,CURLOPT_POSTFIELDS, json_encode($fields));
-
-        // $info = curl_getinfo($ch);
-        // print_r($info);
-        // print_r($info['request_header']);
-
-        $result = curl_exec($ch);
-        curl_close($ch);
-
-        return $result;
-    }
 }
--- src/Utils/NotificationService.php
+++ src/Utils/NotificationService.php
@@ -2,66 +2,20 @@
 
 namespace App\Utils;
 
-//use App\Utils\GeneratorEncuentro;
-
 class NotificationService{
 
+    // ###########################################################################
+    // ########################## CONFIG FIREBASE FCM ############################
     const URL_API_FCM = "https://fcm.googleapis.com/fcm/send";
-    // const TOKEN_ACCES_SERVER_FIREBASE = 'AAAA9P-hCPk:APA91bHBrbHBDxwmtD6RdwK-MvyxV17WUmqsyHV5VTOgTL5hfTbUiiBV2x88Ywqj0Y51di-VaDB7CS2ihHns1Hjj5l-DAcrDseJaWommLVR47y2vMO9Iw4nRYgKTtc77J0ej8nF2QYjr';
     const TOKEN_ACCES_SERVER_FIREBASE = 'AAAAIv70EkE:APA91bGetKCqMPfkVuRLn7nT9qqMgUdzc9mN5lB-ny9_XX1gQjdITjfcFE2NxPC_3I3c43XzwcVb8Y6RvT5I55hhScGpT8zDfkWKdFcXdlTCDhTHJo42ahHF-PI4bhrOcfxUqGrRz-lT';
 
-    public function sendNotificationFCM($titleNotif, $bodyNotif, $tokenDev)
-    {
-        $URL  = "https://fcm.googleapis.com/fcm/send";  //API URL of FCM
-    
-        //$API_ACCESS_KEY_SERVER = 'AAAA9P-hCPk:APA91bHBrbHBDxwmtD6RdwK-MvyxV17WUmqsyHV5VTOgTL5hfTbUiiBV2x88Ywqj0Y51di-VaDB7CS2ihHns1Hjj5l-DAcrDseJaWommLVR47y2vMO9Iw4nRYgKTtc77J0ej8nF2QYjr'; // YOUR_FIREBASE_API_KEY
-        $API_ACCESS_KEY_SERVER = 'AAAAIv70EkE:APA91bGetKCqMPfkVuRLn7nT9qqMgUdzc9mN5lB-ny9_XX1gQjdITjfcFE2NxPC_3I3c43XzwcVb8Y6RvT5I55hhScGpT8zDfkWKdFcXdlTCDhTHJo42ahHF-PI4bhrOcfxUqGrRz-lT';
-        //$TOKEN_DEVICE = 'fOYrDHi4IKQ:APA91bEb_W4aV9AsWQO5clEOJhjhLW362IMyvYAZtOXR4JU--zuwNTfAwrf9Pb3Zg_TWbUek7ctn2bulMOYjL_1c2B4T6f2Rk9fy2qM-x09kx9iR7KsbnVV_Teea-lMwtOZwERAbvloD';
-        $TOKEN_DEVICE = $tokenDev;
-
-        $notif = (object) null;
-        // $notif->title = "Portugal vs. Denmark";
-        // $notif->body = "El cuerpo de la notif";
-        $notif->title = $titleNotif;
-        $notif->body = $bodyNotif;
-
-        $msg = (object) null;
-        $msg->token = $TOKEN_DEVICE;
-        $msg->notification = $notif;
-
-        // dato a mandar
-        $response = (object) null;
-        $response->message = $msg;
-
-        $fields = array('to' => $TOKEN_DEVICE, 'data' => $response);
-
-        // print_r($response);
-        // echo(json_encode($response));
-
-        $headers = array('Authorization: key=' . $API_ACCESS_KEY_SERVER, 'Content-Type: application/json');
-
-        #Send Reponse To FireBase Server    
-        $ch = curl_init(); 
-        curl_setopt($ch,CURLOPT_URL, $URL);
-        curl_setopt($ch,CURLOPT_POST, true);
-        curl_setopt($ch,CURLOPT_HTTPHEADER, $headers);
-        curl_setopt($ch,CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch,CURLOPT_SSL_VERIFYPEER, false);
-        curl_setopt($ch,CURLOPT_POSTFIELDS, json_encode($fields));
-
-        // $info = curl_getinfo($ch);
-        // print_r($info);
-        // print_r($info['request_header']);
-
-        $result = curl_exec($ch);
-        curl_close($ch);
-
-        return $result;
-    }
+    // ###########################################################################
+    // ###########################################################################
 
     // envia una notificacion con titulo y el mensaje recibido al dispositivo correspondiente
     public function sendSimpleNotificationFCM($titleNotif, $tokenDev, $msg)
     {
+        // estructura de la notificacion que se requiere
         $notif = (object) null;
         $notif->title = $titleNotif;
         $notif->body = $msg;
@@ -70,14 +24,11 @@ class NotificationService{
         $msgtest->token = $tokenDev;
         $msgtest->notification = $notif;
 
-        // $data = (object) null;
-        // $data->test = "texto de tokenDev";
-
-        // dato a mandar
+        // dato a mandar, probando mandar mas datos
         $response = (object) null;
         $response->message = $msgtest;
-        // $response->data = $data;
 
+        // especificamos a quien mandar la notificacion y los datos a mandar
         $fields = array('to' => $tokenDev, 'data' => $response);
 
         $headers = array('Authorization: key=' . $this::TOKEN_ACCES_SERVER_FIREBASE, 'Content-Type: application/json');
