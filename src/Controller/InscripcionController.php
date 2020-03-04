5edeb9e701233499ad8c6c6406230d60fb5f89ed
PujolCity <sergio19101992@gmail.com>
2020-03-03 21:52:16 -0300

    recuperar inscripcion implementado

--- src/Controller/InscripcionController.php
+++ src/Controller/InscripcionController.php
@@ -119,123 +119,57 @@ class InscripcionController extends AbstractFOSRestController
         return $response;
     }
 
-    // /**
-    //  * Lista de todos las predios.
-    //  * @Rest\Get("/referees"), defaults={"_format"="json"})
-    //  * 
-    //  * @return Response
-    //  */
-    // public function allReferees()
-    // {
+    /**
+     * Devuelve todas los predios de una competencia
+     * @Rest\Get("/inscription-competition"), defaults={"_format"="json"})
+     * 
+     * @return Response
+     */
+    public function getInscriptionByCompetition(Request $request){
+        $repository = $this->getDoctrine()->getRepository(Inscripcion::class);
+        $repositoryComp = $this->getDoctrine()->getRepository(Competencia::class);
 
-    //     $repository = $this->getDoctrine()->getRepository(Juez::class);
-    //     $judges = $repository->findall();
+        $respJson = (object) null;
 
-    //     $judges = $this->get('serializer')->serialize($judges, 'json', [
-    //         'circular_reference_handler' => function ($object) {
-    //             return $object->getId();
-    //         },
-    //         'ignored_attributes' => ['competencia']
-    //     ]);
+        $idCompetencia = $request->get('idCompetencia');
 
-    //     $response = new Response($judges);
-    //     $response->setStatusCode(Response::HTTP_OK);
-    //     $response->headers->set('Content-Type', 'application/json');
-
-    //     return $response;
-    // }
-
-    // /**
-    //  * Devuelve todas los predios de una competencia
-    //  * @Rest\Get("/referees/competition"), defaults={"_format"="json"})
-    //  * 
-    //  * @return Response
-    //  */
-    // public function getJudgesByCompetition(Request $request){
-    //     $repository = $this->getDoctrine()->getRepository(Juez::class);
-      
-    //     $respJson = (object) null;
-
-    //     $idCompetencia = $request->get('idCompetencia');
-
-    //     // vemos si recibimos algun parametro
-    //     if(!empty($idCompetencia)){
-    //         $judges = $repository->findJudgesByCompetetition($idCompetencia);
-    //         $statusCode = Response::HTTP_OK;
-
-    //         $judges = $this->get('serializer')->serialize($judges, 'json', [
-    //             'circular_reference_handler' => function ($object) {
-    //                 return $object->getId();
-    //             },
-    //             'ignored_attributes' => ['competencia']
-    //         ]);
-    //     }
-    //     else{
-    //         $judges  = NULL;
-    //         $statusCode = Response::HTTP_BAD_REQUEST;
-    //         $respJson->messaging = "Faltan parametros";
-    //     }
+        // vemos si recibimos algun parametro
+        if(!empty($idCompetencia)){
+            $competencia = $repositoryComp->find($idCompetencia);
         
-    //     $respJson = json_decode($judges);
-    //     $respJson = json_encode($respJson);
+            if($competencia == null){
+                $statusCode = Response::HTTP_BAD_REQUEST;
+                $respJson->messaging = "Peticion mal formada. Faltan parametros.";
+                }
+            else{
+                $inscription = $competencia->getInscripcion();
+                $inscription = $this->get('serializer')->serialize($inscription, 'json', [
+                    'circular_reference_handler' => function ($object) {
+                        return $object->getId();
+                    },
+                    'ignored_attributes' => ['competencia', '__initializer__', '__cloner__', '__isInitialized__']
+                ]);
+                $inscripciones = array();
+                $respJson = $inscription;
+                $statusCode = Response::HTTP_OK;
+            }
+        }
+        else{
+            $inscription  = NULL;
+            $statusCode = Response::HTTP_BAD_REQUEST;
+            $respJson->messaging = "Faltan parametros";
+        }
+        
+        $respJson = json_decode($inscription);
+        $respJson = json_encode($respJson);
 
-    //     $response = new Response($respJson);
-    //     $response->setStatusCode(Response::HTTP_OK);
-    //     $response->headers->set('Content-Type', 'application/json');
+        $response = new Response($respJson);
+        $response->setStatusCode(Response::HTTP_OK);
+        $response->headers->set('Content-Type', 'application/json');
     
-    //     return $response;
-    // }
+        return $response;
+    }
     
-
-
-    // /**
-    //  * Crea un usuario.
-    //  * @Rest\Delete("/judge/del"), defaults={"_format"="json"})
-    //  * 
-    //  * @return Response
-    //  */
-    // public function delete(Request $request){
-
-    //     $respJson = (object) null;
-    //     $statusCode;
-
-    //     $idJuez = $request->get('idJuez');
-      
-    //     // vemos si recibimos el id de un predio para eliminarlo
-    //     if(empty($idJuez)){
-    //         $respJson->success = false;
-    //         $statusCode = Response::HTTP_BAD_REQUEST;
-    //         $respJson->messaging = "Peticion mal formada. Faltan parametros.";
-    //     }
-    //     else{
-    //         $repository=$this->getDoctrine()->getRepository(Juez::class);
-    //         $juez = $repository->find($idJuez);
-    //         if($juez == NULL){
-    //             $respJson->success = true;
-    //             $statusCode = Response::HTTP_OK;
-    //             $respJson->messaging = "El juez no existe o ya fue eliminado";
-    //         }
-    //         else{
-    //             // eliminamos el dato y refrescamos la DB
-    //             $em = $this->getDoctrine()->getManager();
-    //             $em->remove($juez);
-    //             $em->flush();
-    
-    //             $respJson->success = true;
-    //             $statusCode = Response::HTTP_OK;
-    //             $respJson->messaging = "Eiminacion correcta";
-    //         }
-    //     }
-
-    //     $respJson = json_encode($respJson);
-
-    //     $response = new Response($respJson);
-    //     $response->headers->set('Content-Type', 'application/json');
-    //     $response->setStatusCode($statusCode);
-  
-    //     return $response;
-    // }
-
     // ######################################################################################
     // ############################ funciones auxiliares ####################################
 
